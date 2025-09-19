798972fe7805aea2b8d293dbe6976a414b9c4792
pwaronofsky <paularonofsky@gmail.com>
2025-09-19 10:13:32 -0400

    initial work for auth0

--- router.js
+++ router.js
@@ -1,44 +1,37 @@
 const apiRouter = require("express").Router()
 const userController = require("./controllers/userController")
-const postController = require("./controllers/postController")
-const followController = require("./controllers/followController")
 const bracketController = require("./controllers/bracketController")
 const cors = require("cors")
+
+const { auth } = require('express-oauth2-jwt-bearer');
+
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: 'https://dev-c15du7wevqtpe3bb.us.auth0.com/api/v2/',
+  issuerBaseURL: 'https://dev-c15du7wevqtpe3bb.us.auth0.com/',
+});
 
 apiRouter.use(cors())
 
 apiRouter.get("/", (req, res) => res.json("Hello, if you see this message that means your backend is up and running successfully. Congrats! Now let's continue learning React!"))
 
 // check token to log out front-end if expired
-apiRouter.post("/checkToken", userController.checkToken)
+apiRouter.post("/checkToken", checkJwt)
 
-apiRouter.post("/getHomeFeed", userController.apiMustBeLoggedIn, userController.apiGetHomeFeed)
+
 apiRouter.post("/register", userController.apiRegister)
 apiRouter.post("/login", userController.apiLogin)
-apiRouter.get("/post/:id", postController.reactApiViewSingle)
-apiRouter.post("/post/:id/edit", userController.apiMustBeLoggedIn, postController.apiUpdate)
-apiRouter.delete("/post/:id", userController.apiMustBeLoggedIn, postController.apiDelete)
-apiRouter.post("/create-post", userController.apiMustBeLoggedIn, postController.apiCreate)
-apiRouter.post("/search", postController.search)
 
-apiRouter.post("/doesUsernameExist", userController.doesUsernameExist)
-apiRouter.post("/doesEmailExist", userController.doesEmailExist)
-
-// profile related routes
-apiRouter.post("/profile/:username", userController.ifUserExists, userController.sharedProfileData, userController.profileBasicData)
-apiRouter.get("/profile/:username/posts", userController.ifUserExists, userController.apiGetPostsByUsername)
-apiRouter.get("/profile/:username/followers", userController.ifUserExists, userController.profileFollowers)
-
-// follow routes
-apiRouter.post("/addFollow/:username", userController.apiMustBeLoggedIn, followController.apiAddFollow)
-apiRouter.post("/removeFollow/:username", userController.apiMustBeLoggedIn, followController.apiRemoveFollow)
+// apiRouter.post("/doesUsernameExist", userController.doesUsernameExist)
+// apiRouter.post("/doesEmailExist", userController.doesEmailExist)
 
 //bracket routes
-apiRouter.post("/bracket/update-create", userController.apiMustBeLoggedIn, bracketController.apiUpdateCreate)
-apiRouter.post("/bracket/get", userController.apiMustBeLoggedIn, bracketController.apiGet)
-apiRouter.post("/bracket/canEdit", userController.apiMustBeLoggedIn, bracketController.apiCanEdit)
+apiRouter.post("/bracket/update-create", checkJwt, bracketController.apiUpdateCreate)
+apiRouter.post("/bracket/get", checkJwt, bracketController.apiGet)
+apiRouter.post("/bracket/canEdit", checkJwt, bracketController.apiCanEdit)
 
 //standings routes
-apiRouter.post("/getStandings", userController.apiMustBeLoggedIn, userController.getStandings);
+apiRouter.post("/getStandings", checkJwt, userController.getStandings);
 
 module.exports = apiRouter
